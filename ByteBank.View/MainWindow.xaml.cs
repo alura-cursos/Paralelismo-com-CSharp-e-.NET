d7075d6faf994f0791bc1cfcb435e359696a0aa7
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2017-06-02 17:23:56 -0300

    Fim da Aula1

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -36,18 +36,64 @@ namespace ByteBank.View
         {
             var contas = r_Repositorio.GetContaClientes();
 
+            var contasQuantidadePorThread = contas.Count() / 4;
+
+            var contas_parte1 = contas.Take(contasQuantidadePorThread);
+            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
+            var contas_parte3 = contas.Skip(contasQuantidadePorThread*2).Take(contasQuantidadePorThread);
+            var contas_parte4 = contas.Skip(contasQuantidadePorThread*3);
+
             var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            foreach (var conta in contas)
+            Thread thread_parte1 = new Thread(() =>
             {
-                var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                resultado.Add(resultadoConta);
-            }
+                foreach (var conta in contas_parte1)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte2 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte3 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte3)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+            Thread thread_parte4 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte4)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
 
+            thread_parte1.Start();
+            thread_parte2.Start();
+            thread_parte3.Start();
+            thread_parte4.Start();
+
+            while (thread_parte1.IsAlive || thread_parte2.IsAlive
+                || thread_parte3.IsAlive || thread_parte4.IsAlive )
+            {
+                Thread.Sleep(250);
+                //Não vou fazer nada
+            }
+            
             var fim = DateTime.Now;
 
             AtualizarView(resultado, fim - inicio);
